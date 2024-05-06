a76f2472f98a177068fab7b60031741f02c69828
Torin Nelson <nelsontorin1@gmail.com>
2024-05-06 01:02:33 -0700

    asdfasdf

--- zipzip_tree.py
+++ zipzip_tree.py
@@ -132,10 +132,12 @@ class ZipZipTree:
 				cur = cur.left
 			else:
 				cur = cur.right
-    
-		if cur.val and cur.val[0] and isinstance(cur.val[0], list):
-			if len(cur.val[0]) > 1:
-				val = cur.val[0].pop()
+		try:
+			if cur.val and cur.val[0] and isinstance(cur.val[0], list):
+				if len(cur.val[0]) > 1:
+					val = cur.val[0].pop()
+		except Exception:
+			pass
 
 		left = cur.left
 		right = cur.right
